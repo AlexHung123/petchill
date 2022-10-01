22ef73a7b99e68de961791365115789e3c6d7dea
alexhong <admin@holmantai.net>
2022-10-01 20:58:50 +0800

    -initial project

---
+++ petchill-product/src/main/java/com/alexhong/petchill/product/PetchillProductApplication.java
@@ -0,0 +1,13 @@
+package com.alexhong.petchill.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetchillProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetchillProductApplication.class, args);
+    }
+
+}
---
+++ petchill-product/src/test/java/com/alexhong/petchill/product/PetchillProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexhong.petchill.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetchillProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
