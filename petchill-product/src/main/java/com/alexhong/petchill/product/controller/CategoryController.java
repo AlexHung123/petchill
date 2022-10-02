f9e2f75379fd977deacfc04e8ab182efa254878d
alexhong <admin@holmantai.net>
2022-10-03 00:59:24 +0800

    - get level1 categories and its sub-categories

--- petchill-product/src/main/java/com/alexhong/petchill/product/controller/CategoryController.java
+++ petchill-product/src/main/java/com/alexhong/petchill/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.alexhong.petchill.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * Get all categories and sub-categories
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+        List<CategoryEntity> entities = categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("page", entities);
     }
 
 
--- petchill-product/src/main/java/com/alexhong/petchill/product/service/impl/CategoryServiceImpl.java
+++ petchill-product/src/main/java/com/alexhong/petchill/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.alexhong.petchill.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,32 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1. get all categories
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+
+        //2. tree relation
+        //2.1 find all level 1 category
+        List<CategoryEntity> level1Menu = categoryEntities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0).
+                map((menu)->{
+                    menu.setChildren(getChildren(menu, categoryEntities));
+                    return menu;
+                }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+        return level1Menu;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId()).map(categoryEntity -> {
+            //1. find the sub-categories
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+
+        return children;
     }
 
 }
